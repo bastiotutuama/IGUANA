5a9a4304939f39e6769c88f22340d6d50fe605b9
Sebastian Otutuama <mail@basti.io>
2020-09-02 16:38:42 +0200

    basic empty BQLStatement implementation

--- iguana.synthesizer/src/main/java/org/aksw/iguana/syn/model/impl/BQLStatement.java
+++ iguana.synthesizer/src/main/java/org/aksw/iguana/syn/model/impl/BQLStatement.java
@@ -5,4 +5,15 @@ import org.aksw.iguana.syn.model.Statement;
 
 public class BQLStatement extends AbstractStatement implements Statement {
 
+    public String getSubject() {
+        return null;
+    }
+
+    public String getPredicate() {
+        return null;
+    }
+
+    public String getObject() {
+        return null;
+    }
 }
