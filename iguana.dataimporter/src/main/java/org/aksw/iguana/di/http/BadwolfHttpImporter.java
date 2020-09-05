17baaa2c90ed9724abf5384bbbbbea599ddc3d88
Sebastian Otutuama <mail@basti.io>
2020-09-05 10:44:52 +0200

    added argument read-in and defaults

--- iguana.dataimporter/src/main/java/org/aksw/iguana/di/http/BadwolfHttpImporter.java
+++ iguana.dataimporter/src/main/java/org/aksw/iguana/di/http/BadwolfHttpImporter.java
@@ -40,10 +40,10 @@ public class BadwolfHttpImporter {
                 try (ResponseBody responseBody = response.body()) {
                     if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
-                    Headers responseHeaders = response.headers();
+                    /*Headers responseHeaders = response.headers();
                     for (int i = 0, size = responseHeaders.size(); i < size; i++) {
                         System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
-                    }
+                    }*/
 
                     System.out.println(responseBody.string());
                     System.out.println();
--- iguana.synthesizer/src/main/java/org/aksw/iguana/syn/controller/MainController.java
+++ iguana.synthesizer/src/main/java/org/aksw/iguana/syn/controller/MainController.java
@@ -17,21 +17,28 @@ import java.util.ArrayList;
 public class MainController {
 
     private static final String defaultOutputMethod = "endpoint";
-    private static final String defaultInputFilePath = "/home/otutuama/datasets/swdfu8.nt"; ///Users/sebastian/Dropbox/Academic Education/Uni Paderborn/Bachelor Thesis/Datasets and Queries/SWDF/Dataset/swdfu8_5000.nt
+    private static final String defaultInputFilePath = "/Users/sebastian/Dropbox/Academic Education/Uni Paderborn/Bachelor Thesis/Datasets and Queries/SWDF/Dataset/swdfu8_5000.nt"; //"/home/otutuama/datasets/swdfu8.nt";
+    private static final String defaultEndpointAdress = "http://131.234.29.241:1234/bql"; //"http://localhost:1234/bql";
     private static final String defaultGraphName = "swdf";
-    private static final String defaultEndpointAdress = "http://localhost:1234/bql"; //"http://131.234.29.241:1234/bql
     private static final String defaultOutputFilePath = "/home/otutuama/datasets/swdfu8.bql"; //"/Users/sebastian/Dropbox/Academic Education/Uni Paderborn/Bachelor Thesis/Datasets and Queries/SWDF/Dataset/swdfu8_5000.bql"
     private static final int defaultChunkSize = 5;
 
 
     public static void main(String [] args) {
 
-        String outputMethod = defaultOutputMethod;
-        String inputFilePath = defaultInputFilePath;
-        String graphName = defaultGraphName;
-        String endpointAdress= defaultEndpointAdress;
-        String outputFilePath = defaultOutputFilePath;
-        int chunkSize = defaultChunkSize;
+        String outputMethod;
+        String inputFilePath;
+        String graphName;
+        String endpointAdress;
+        String outputFilePath;
+        int chunkSize;
+
+        outputMethod = setVariableFromInputArguments(args, 0, defaultOutputMethod);
+        inputFilePath = setVariableFromInputArguments(args, 1, defaultInputFilePath);
+        endpointAdress = setVariableFromInputArguments(args, 2, defaultEndpointAdress);
+        outputFilePath = setVariableFromInputArguments(args, 2, defaultOutputFilePath);
+        graphName = setVariableFromInputArguments(args, 3, defaultGraphName);
+        chunkSize = Integer.parseInt(setVariableFromInputArguments(args, 4, String.valueOf(defaultChunkSize)));
 
         ArrayList<Statement> rdfNtripleStatements = FileParser.readInStatementsFromFile(inputFilePath, FileParser.SupportedInputLanguage.N_TRIPLE);
 
@@ -44,6 +51,13 @@ public class MainController {
         }
     }
 
+    private static String setVariableFromInputArguments(String[] arguments, int argumentIndex, String defaultValue){
+        if(arguments.length >= argumentIndex + 1 && !arguments[argumentIndex].isEmpty()) {
+            return arguments[argumentIndex];
+        } else {
+            return defaultValue;
+        }
+    }
 
     private static void synthesizeRdfNtripleStatmentListToBqlAndWriteToFile(ArrayList<Statement> rdfNtripleStatements, String outputFilePath){
 
@@ -75,6 +89,7 @@ public class MainController {
             currentBqlInsertQueriesChunk = RDFNtripleStatementToBadwolfStatementSythesizer.generateBQLInsertQueryFromRDFNtripleStatements(graphName, rdfNtripleStatements.subList(i, i + chunkSize - 1));
             sendBqlInsertQueryListToBqlEndpoint(endpointAdress,currentBqlInsertQueriesChunk);
             i+=chunkSize;
+            //System.out.println("Approximate load-progress:" + i  * 100 / rdfNtripleStatements.size() + "%");
         }
     }
 
