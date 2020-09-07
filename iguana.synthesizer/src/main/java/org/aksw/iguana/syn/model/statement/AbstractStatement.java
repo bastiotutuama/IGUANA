d86391d4407f880ff04eef657d81467b5e495eec
Sebastian Otutuama <mail@basti.io>
2020-09-07 22:32:22 +0200

    Refactoring: Statement Identifier-Enums into Statement-Interface.

--- iguana.synthesizer/src/main/java/org/aksw/iguana/syn/model/statement/AbstractStatement.java
+++ iguana.synthesizer/src/main/java/org/aksw/iguana/syn/model/statement/AbstractStatement.java
@@ -5,69 +5,17 @@ import java.util.List;
 
 public abstract class AbstractStatement implements Statement{
 
-    public enum StatementPartIdentifier {
-        SUBJECT,
-        PREDICATE,
-        OBJECT
-    }
-
-    public enum StatementLanguage {
-        RDF_NTRIPLE("N-TRPLE"),
-        BQL_INSERT("");
-
-        private final String jenaLanguageIdentifier;
-
-        /**
-         * @param jenaLanguageIdentifier
-         */
-        StatementLanguage(final String jenaLanguageIdentifier) {
-            this.jenaLanguageIdentifier = jenaLanguageIdentifier;
-        }
-
-        /* (non-Javadoc)
-         * @see java.lang.Enum#toString()
-         */
-        @Override
-        public String toString() {
-            return jenaLanguageIdentifier;
-        }
-    }
-
-    public enum StatementControlSymbol {
-        URI_NODE_PROTOCOL_COLON,
-        URI_NODE_SLASH,
-        URI_NODE_FULLSTOP,
-        URI_NODE_OPENING_BRACKET,
-        URI_NODE_CLOSING_BRACKET,
-        URI_ILLEGAL_PERCENT_WHICH_WILL_NOT_ESCAPE_IN_INSERT_HTTP_REQUEST,
-        URI_PREDICATE_OPENING_BRACKET,
-        URI_PREDICATE_CLOSING_BRACKET,
-        URI_PREDICATE_PROTOCOL_COLON,
-        URI_PREDICATE_SLASH,
-        URI_PREDICATE_FULLSTOP,
-        LITERAL_OPENING_BRACKET,
-        LITERAL_CLOSING_BRACKET,
-        LITERAL_DATATYPE_DELIMETER,
-        LITERAL_DATATYPE_SPECIFIER_TEXT,
-        LITERAL_DATATYPE_SPECIFIER_INTEGER,
-        LITERAL_DATATYPE_SPECIFIER_FLOAT,
-        LITERAL_DATATYPE_SPECIFIER_BOOLEAN,
-        LITERAL_DATATYPE_SPECIFIER_BLOB
-    }
-
-
-
-    //TODO enable inhertance fo constrolSymbol Methods
+    //TODO enable inhertance for constrolSymbol Methods
 
     public static List<StatementControlSymbol> getStatemenControlSymbolIdentifiers () {
         return Arrays.asList(StatementControlSymbol.class.getEnumConstants());
     }
 
-    //TODO enable inhertance fo constrolSymbol Methods
+    //TODO enable inhertance for constrolSymbol Methods
 
     // public abstract String getStatementControlSymbol (StatementControlSymbol statementControlSymbol);
 
-    //TODO enable inhertance fo constrolSymbol Methods
+    //TODO enable inhertance for constrolSymbol Methods
 
     // public abstract boolean doesStatementControlSymbolExistForString(String controlSymbolCandidate);
 
--- iguana.synthesizer/src/main/java/org/aksw/iguana/syn/model/statement/Statement.java
+++ iguana.synthesizer/src/main/java/org/aksw/iguana/syn/model/statement/Statement.java
@@ -1,6 +1,57 @@
 package org.aksw.iguana.syn.model.statement;
 
 public interface Statement {
+
+    public enum StatementPartIdentifier {
+        SUBJECT,
+        PREDICATE,
+        OBJECT
+    }
+
+    public enum StatementLanguage {
+        RDF_NTRIPLE("N-TRPLE"),
+        BQL_INSERT("");
+
+        private final String jenaLanguageIdentifier;
+
+        /**
+         * @param jenaLanguageIdentifier
+         */
+        StatementLanguage(final String jenaLanguageIdentifier) {
+            this.jenaLanguageIdentifier = jenaLanguageIdentifier;
+        }
+
+        /* (non-Javadoc)
+         * @see java.lang.Enum#toString()
+         */
+        @Override
+        public String toString() {
+            return jenaLanguageIdentifier;
+        }
+    }
+
+    public enum StatementControlSymbol {
+        URI_NODE_PROTOCOL_COLON,
+        URI_NODE_SLASH,
+        URI_NODE_FULLSTOP,
+        URI_NODE_OPENING_BRACKET,
+        URI_NODE_CLOSING_BRACKET,
+        URI_ILLEGAL_PERCENT_WHICH_WILL_NOT_ESCAPE_IN_INSERT_HTTP_REQUEST,
+        URI_PREDICATE_OPENING_BRACKET,
+        URI_PREDICATE_CLOSING_BRACKET,
+        URI_PREDICATE_PROTOCOL_COLON,
+        URI_PREDICATE_SLASH,
+        URI_PREDICATE_FULLSTOP,
+        LITERAL_OPENING_BRACKET,
+        LITERAL_CLOSING_BRACKET,
+        LITERAL_DATATYPE_DELIMETER,
+        LITERAL_DATATYPE_SPECIFIER_TEXT,
+        LITERAL_DATATYPE_SPECIFIER_INTEGER,
+        LITERAL_DATATYPE_SPECIFIER_FLOAT,
+        LITERAL_DATATYPE_SPECIFIER_BOOLEAN,
+        LITERAL_DATATYPE_SPECIFIER_BLOB
+    }
+
     /*
     hand over class and implement returns here
     public static String getStatementControlSymbol (AbstractStatement.StatementControlSymbol statementControlSymbol){
